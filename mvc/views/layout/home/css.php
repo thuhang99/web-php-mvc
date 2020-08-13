c5f66b202a30c790d8dd88e9586bcad1d500a098
Phạm Thu Hằng <phamthuhang09091999@gmail.com>
2020-08-13 21:25:57 +0700

    update view

---
+++ mvc/views/layout/home/css.php
@@ -0,0 +1,14 @@
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/layout/css/bootstrap.min.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/layout/css/font-awesome.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/layout/css/owl.carousel.css">
+    <link rel="stylesheet" href="assets/layout/css/style.css">
+    <link rel="stylesheet" href="assets/layout/css/responsive.css">
---
+++ mvc/views/layout/home/js.php
@@ -0,0 +1,18 @@
+<script src="https://code.jquery.com/jquery.min.js"></script>
+    
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
+    <!-- jQuery sticky menu -->
+    <script src="assets/layout/js/owl.carousel.min.js"></script>
+    <script src="assets/layout/js/jquery.sticky.js"></script>
+    
+    <!-- jQuery easing -->
+    <script src="assets/layout/js/jquery.easing.1.3.min.js"></script>
+    
+    <!-- Main Script -->
+    <script src="assets/layout/js/main.js"></script>
+    
+    <!-- Slider -->
+    <script type="text/javascript" src="assets/layout/js/bxslider.min.js"></script>
+	<script type="text/javascript" src="assets/layout/js/script.slider.js"></script>
